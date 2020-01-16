a693f4cdc19cb7d0a630f7a50ff17bd3207abd3c
Jetty <kmclo@naver.com>
2020-01-17 07:43:08 +0900

    #8 Conclusions

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Maven+Pro');
+  @import url('https://fonts.googleapis.com/css?family=Maven+Pro:400,500');
   ${reset};
   body{
     font-family: 'Maven Pro', sans-serif;
